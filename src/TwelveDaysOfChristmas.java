e5eddf99e22a3d5aeb753171929fefd73c7318ac
MissHarah <motunrayor2@gmail.com>
2022-12-30 10:14:25 +0100

    first commit

--- src/TwelveDaysOfChristmas.java
+++ src/TwelveDaysOfChristmas.java
@@ -1,56 +1,46 @@
-//public class TwelveDaysOfChristmas {
+public class TwelveDaysOfChristmas {
 
-//    public static void main(String[] args) {
 
-//        int input = 1;
-//        while (input <= 12) {
+            String setDays;{
 
-//            switch (count) ;
-//            case day1 = "On the first day of Christmas my true love sent to me"
-//        }
-//        {
-//            case day2 = "On the second day of Christmas, my true love sent to me"
-//        }
-//
-//            {
-//                case day3 = "On the third day of Christmas my true love sent to me"
-//            }
-//        {
-//            case day4 = "On the fourth day of Christmas, my true love sent to me"
-//        }
-//        {
-//            case day5 = "On the fifth day of Christmas, my true love sent to me"
-//        }
-//        {
-//            case day6 = "On the sixth day of Christmas, my true love sent to me"
-//        }
-//        {
-//            case day7 = "On the seventh day of Christmas, my true love sent to me"
-//        }
-//        {
-//            case day8 = "On the eighth day of Christmas, my true love sent to me"
-//        }
-//        {
-//            case day9 = "On the ninth day of Christmas, my true love sent to me"
-//        }
-//        {
-//            case day10 = "On the tenth day of Christmas, my true love sent to me"
-//        }
-//        {
-//            case day11 = "On the eleventh day of Christmas, my true love sent to me"
-//        }
-//        {
-//            case day12 = "on the twelve day of christmas, my true love sent to me"
-//        }
-//
-//                String day = 1;
-//
-//                Switch(count);
-//            case day1 = X
-//
-//                public String setDays ( int i = 1 <= 12){
-//            }
-//
-//        }
-//    }
-//
+                int countDays = 12;
+                switch (countDays) {
+                    case 1:
+                        System.out.println("On the first day of Christmas my true love sent to me");
+                    case 2:
+                        System.out.println("On the second day of Christmas, my true love sent to me");
+                    case 3:
+                        System.out.println("On the third day of Christmas my true love sent to me");
+                    case 4:
+                        System.out.println("On the fourth day of Christmas, my true love sent to me");
+                    case 5:
+                        System.out.println("On the fifth day of Christmas, my true love sent to me");
+                    case 6:
+                        System.out.println("On the sixth day of Christmas, my true love sent to me");
+                    case 7:
+                        System.out.println("On the seventh day of Christmas, my true love sent to me");
+                    case 8:
+                        System.out.println("On the eighth day of Christmas, my true love sent to me");
+                    case 9:
+                        System.out.println("On the ninth day of Christmas, my true love sent to me");
+                    case 10:
+                        System.out.println("On the tenth day of Christmas, my true love sent to me");
+                    case 11:
+                        System.out.println("On the eleventh day of Christmas, my true love sent to me");
+                    case 12:
+                        System.out.println("on the twelve day of christmas, my true love sent to me");
+
+
+                }
+
+            }
+
+            {
+            }
+        }
+
+
+
+
+
+
---
+++ test/TwelveDaysOfChristmas.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TwelveDaysOfChristmas {
+
+    public static void main(String[] args) {
+
+        int input = 1;
+        while (input <= 12) {
+
+            Scanner song =new Scanner(System.in);
+        }
+    }
+}
